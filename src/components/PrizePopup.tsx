f952ed0d3484f3b237bec89fa90523b76a833bc1
tienphat2910 <21032191.phat@student.iuh.edu.vn>
2025-09-08 20:49:16 +0700

    fix link

--- src/components/PrizePopup.tsx
+++ src/components/PrizePopup.tsx
@@ -116,7 +116,7 @@ export default function PrizePopup({
         } catch {}
       }
       window.open(
-        `https://m.me/xyz.63631?text=${encodeURIComponent(code)}`,
+        `https://m.me/tianlonghcm?text=${encodeURIComponent(code)}`,
         "_blank"
       );
     }
